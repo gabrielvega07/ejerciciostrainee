88b09b688383bb09cb7736030c32fc17713324cd
gabrielvega07 <choj2015pablo@gmail.com>
2023-04-28 10:36:48 -0600

    Se presenta la solucion del ejercicio 28.4

---
+++ Ejercicio 28.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_28._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+            var Saludo = "Hola";
+
+            Console.WriteLine("Ingrese su nombre:");
+            Nombre = Console.ReadLine();
+            Console.WriteLine(Saludo + ", " + Nombre);
+            Console.ReadKey();
+        }
+    }
+}
