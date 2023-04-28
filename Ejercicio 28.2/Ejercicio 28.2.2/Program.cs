abcae70ac38f0f00962d50e1e336bbd8b9bb4a8c
gabrielvega07 <choj2015pablo@gmail.com>
2023-04-28 10:35:24 -0600

    Se presenta la sulucion de los ejercicios 28.2

---
+++ Ejercicio 28.2/Ejercicio 28.2.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio_.Net_Core_2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Name = "Pablo";
+            Version dotnetVersion = Environment.Version;
+
+            Console.WriteLine($"Hola {Name} este es un proyecto de .Net Core en su Version {dotnetVersion}");
+            Console.ReadKey();
+        }
+    }
+}
