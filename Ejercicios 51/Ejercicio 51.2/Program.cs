8d13ba48e3626d673912c04226838a746a85df6b
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-04 16:57:33 -0600

    Se presente la solucion del ejercicio 51.2

---
+++ Ejercicios 51/Ejercicio 51.2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero1, numero2;
+
+Console.WriteLine("Ingrese un numero: ");
+numero1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese un numero: ");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+if (numero1 > numero2)
+{
+    Console.WriteLine("El numero mayor es: " + numero1);
+}
+else
+{
+    Console.WriteLine("El numero mayor es: " + numero2);
+}
+
+Console.ReadKey();
