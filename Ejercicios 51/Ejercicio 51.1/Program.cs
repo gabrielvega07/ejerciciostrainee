908abd039d69560320c65032bee01bf8a20de0aa
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-04 16:56:17 -0600

    Se presenta la sulucion del ejercicio 51.1

---
+++ Ejercicios 51/Ejercicio 51.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int numero1 = 5, numero2 = 10, numero3 = 5;
+int resultado;
+
+resultado = numero1 + numero2 + numero3;
+
+Console.WriteLine("El reusltado es: " + resultado);
+
+Console.ReadLine();
+
