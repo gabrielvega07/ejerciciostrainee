638056ed28df7a0bd0e4453b3405018dd2ee83b9
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-04 16:58:31 -0600

    Se presente la solucion del ejercicio 51.8

---
+++ Ejercicios 51/Ejercicio 51.8/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0 || i % 3 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+Console.ReadKey();
