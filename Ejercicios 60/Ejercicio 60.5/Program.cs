f156492e6dd6e612378ab113a50de9f95d7e2b47
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-05 09:56:01 -0600

    Se presenta la solucion del ejercicio 60.5

---
+++ Ejercicios 60/Ejercicio 60.5/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Introduzca un número: ");
+int numero = int.Parse(Console.ReadLine());
+
+ImprimirTabla(numero);
+
+
+static void ImprimirTabla(int numero)
+{
+    Console.WriteLine($"Labla de multiplicar del {numero} es:");
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine($"{numero} x {i} = {numero * i}");
+    }
+}
+
+Console.ReadKey();
