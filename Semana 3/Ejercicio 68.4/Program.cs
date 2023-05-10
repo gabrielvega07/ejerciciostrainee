34d939140393b8c09cc9da36532961cbdd121bcb
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-10 13:42:22 -0600

    Se presenta la solucion del ejercicio 68.4

---
+++ Semana 3/Ejercicio 68.4/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.WriteLine("Introducir el primer numero: ");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introducir el Segundo numero: ");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introducir el Tercer numero: ");
+int numero3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introducir el Cuarto numero: ");
+int numero4 = Convert.ToInt32(Console.ReadLine());
+
+StringBuilder sb = new StringBuilder();
+sb.Append("El primer numero introducido es el ");
+sb.Append(numero1);
+
+sb.Append(", despues se han introducido el ");
+sb.Append(numero2);
+
+sb.Append(" y ");
+sb.Append(numero3);
+
+sb.Append(" y por ultimo el numero ");
+sb.Append(numero4);
+
+Console.WriteLine(sb.ToString());   
+Console.ReadLine();
