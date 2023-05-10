be8062a32ce0037d4540e7b1c10857263b39e2c6
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-10 13:42:51 -0600

    Se presenta la solucion del ejercicio 68.2

---
+++ Semana 3/Ejercicio 68.2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ingrese una palabra: ");
+string palabra = Console.ReadLine();
+
+//convertir la palabra en un arreglo de carcteres
+char[] caracteresEvaluados = palabra.ToCharArray();
+
+//revertir el arreglo de caracteres para formar la nueva palabra   
+Array.Reverse(caracteresEvaluados);                  
+
+
+string palabraInvertida = new string(caracteresEvaluados);
+Console.WriteLine($"La palabra invertida es: " + palabraInvertida);
+
+Console.ReadKey();
