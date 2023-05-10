7f326a4daeabddb9643e0f2631e188cba62ed1b1
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-10 13:42:35 -0600

    Se presenta la solucion del ejercicio 68.5

---
+++ Semana 3/Ejercicio 68.5/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+char[] letras = new char['Z' - 'A' + 1];
+
+for (int i = 0, letra = 'Z'; i < letras.Length; i++, letra--)
+{
+    letras[i] = (char)letra;
+}
+
+string cadena = String.Join(" ", letras);
+
+Console.WriteLine($"La cadena de las letras de la Z a la A es: \n{cadena}");
+   
+
+Console.ReadKey();
