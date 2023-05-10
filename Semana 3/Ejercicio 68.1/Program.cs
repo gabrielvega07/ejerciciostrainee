4fe117144435c48799518eae276423fd1915839f
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-10 13:41:53 -0600

    Se presenta la solucion del ejercicio 68.1

---
+++ Semana 3/Ejercicio 68.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+
+if (nombre.ToLower() == "alejandro")
+{
+    Console.WriteLine("Hola Alejandro");
+}
+
+else
+{
+    Console.WriteLine("No te conozco");
+}
+
+Console.ReadKey();
