65c02cdbab669b628f52600261412d20aabb3265
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-11 20:46:17 -0600

    Se presenta la sulucion del Ejercicio Semana 2

---
+++ Semana 2/Ejercicio Semana 2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Ingresar una cadena: ");
+string cadena = Console.ReadLine();
+if (EsPalindromo(cadena))
+{
+    Console.WriteLine($"{cadena} Si es palíndromo");
+}
+else
+{
+    Console.WriteLine($"{cadena} No es palíndromo");
+}
+
+static bool EsPalindromo(string cadena)
+{
+    // Eliminar espacios en blanco y convertir a minúsculas para comparar 
+    cadena = cadena.Replace(" ", "").ToLower();
+    // Comparar la cadena original con la cadena invertida
+    return cadena == new string(cadena.Reverse().ToArray());
+}
+
+Console.ReadKey();
