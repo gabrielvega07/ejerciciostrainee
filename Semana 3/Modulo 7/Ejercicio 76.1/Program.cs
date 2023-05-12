3ae97cac10b08ce0150b49ec05642959c5155b70
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-12 17:12:53 -0600

    Sepresenta la solucion del ejercicio 76.

---
+++ Semana 3/Modulo 7/Ejercicio 76.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<int> numeros = new List<int>();
+
+// Recorrer del 1 a 1000 y agregarlos a la lista 
+for (int i = 1; i <= 1000; i++)
+{
+    numeros.Add(i);
+
+    //Calculamos la suma de los números en la lista hasta la iteración actual
+    int suma = 0;
+    for (int j = 0; j < numeros.Count; j++)
+    {
+        suma += numeros[j];
+    }
+    Console.WriteLine(suma);
+}
+
+    Console.ReadKey();
+
