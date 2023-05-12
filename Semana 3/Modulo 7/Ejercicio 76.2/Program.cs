363c63437ce14e7e30b0680ff46ab4d945bf7486
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-12 17:13:23 -0600

    Se presenta la solucion del ejercicio76.2

---
+++ Semana 3/Modulo 7/Ejercicio 76.2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+List<string> nombres = new List<string>();
+
+for  (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine("Ingrese un nombre: ");
+    string nombre = Console.ReadLine();
+    nombres.Add(nombre);
+}
+
+Console.WriteLine("Ingrese el nombre que desea buscar: ");
+string nombreBuscado = Console.ReadLine();  
+
+if (nombres.Contains(nombreBuscado))
+{
+    Console.WriteLine($"El nombre {nombreBuscado} si se encuentra en la lista");
+}
+else
+{
+    Console.WriteLine($"El nombre {nombreBuscado} no se encuentra en la lista");
+}
+
+Console.ReadLine();
+
