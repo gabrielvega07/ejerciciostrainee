f2350119d142e8f170b34c3250d0cf4f585a6a1d
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-12 17:13:41 -0600

    Se presenta la solucion del ejercicio76.4

---
+++ Semana 3/Modulo 7/Ejercicio 76.4/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<int> numeros = new List<int>();
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Ingresar el numero {i}:");
+    int numero = int.Parse( Console.ReadLine() );
+    numeros.Add( numero );  
+}
+
+int maximo = numeros[0];
+int minimo = numeros[0];
+
+for (int i = 1; i < numeros.Count; i++)
+{
+    if(numeros[i] > maximo)
+    {
+        maximo = numeros[i];
+    }
+
+    if (numeros[i] < minimo) 
+    { 
+        minimo = numeros[i]; 
+    }
+}
+
+Console.WriteLine($"El numero maximo es {maximo}.");
+Console.WriteLine($"El numero minimo es {minimo}.");
+
+Console.ReadLine();
+
+
