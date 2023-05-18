e25c9f996f2e6c8e8c705f5b2713e0163d1c8c94
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-18 10:35:28 -0600

    Se presenta la solucion del Ejercicio Semana 3

---
+++ Semana 3/Ejercicio Semana3/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using EjercicioSemana3;
+//using System.ComponentModel;
+
+List<clsAlumno> listaAlumnos = new List<clsAlumno>();
+
+listaAlumnos.Add(new clsAlumno("Juan", "Perez", 001, "Ingenieria", new List<double> { 5, 5, 5, 5, 5 }));
+listaAlumnos.Add(new clsAlumno("María", "González", 002, "Medicina", new List<double> { 9, 9, 9, 9, 9 }));
+listaAlumnos.Add(new clsAlumno("Carlos", "López", 003, "Arquitectura", new List<double> { 7, 7, 7, 7, 7 }));
+listaAlumnos.Add(new clsAlumno("Laura", "Rodríguez", 004, "Derecho", new List<double> { 6, 5, 7, 6, 6 }));
+listaAlumnos.Add(new clsAlumno("Luis", "Martínez", 005, "Economía", new List<double> { 8, 7, 6, 8, 9 }));
+
+//clsAlumno alumno1 = new("Juan", "Perez", "001", new List<double> (5, 5, 5, 5, 5));
+
+Console.WriteLine("Lista completa de estudiantes: ");
+
+foreach(clsAlumno alumno in listaAlumnos)
+{
+    double promedio = alumno.CalcularPromedio();
+    Console.WriteLine("Nombre completo: {0} {1}", alumno.Nombre, alumno.Apellido);
+    Console.WriteLine("Numero de Identificacion: {0}", alumno.NumeroIdentificacion);
+    Console.WriteLine("Carrera: {0}", alumno.Carrera);
+    Console.WriteLine("Promedio del alumno: {0}\n", promedio);
+}
+
+Console.WriteLine("Estudiantes con promedio igual o mayor a 6: ");
+foreach (clsAlumno alumno in listaAlumnos)
+{
+    double promedio = alumno.CalcularPromedio();
+    if (promedio >= 6)
+    {
+        Console.WriteLine("Nombre completo: {0} {1}", alumno.Nombre, alumno.Apellido);
+        Console.WriteLine("Numero de Identificacion: {0}\n", alumno.NumeroIdentificacion);
+    }
+}
+
+
+Console.ReadLine();
+
+
---
+++ Semana 3/Ejercicio Semana3/clsAlumno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSemana3
+{
+    internal class clsAlumno
+    {
+        public string Nombre {  get; set; }
+        public string Apellido { get; set; }
+        public int NumeroIdentificacion { get; set; }
+        public string Carrera { get; set; }
+        public List<double> Notas { get; set;}
+
+        public clsAlumno(string nombre, string apellido, int numeroIdentificacion,  string carrera, List<double> notas)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            NumeroIdentificacion = numeroIdentificacion;
+            Carrera = carrera;
+            Notas = notas;
+        }
+
+        public double CalcularPromedio()
+        {
+            double sumarNotas = 0;
+            foreach (var nota in Notas)
+            {
+                sumarNotas += nota;
+            }
+            return sumarNotas / Notas.Count;
+        }
+    }
+}
