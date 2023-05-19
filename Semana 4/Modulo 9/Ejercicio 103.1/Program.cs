0b2e6098509d421ef2f1a4ecd8b8fdcbcf17120d
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-19 09:43:38 -0600

    Se presenta la solucion del ejercicio 103.1

---
+++ Semana 4/Modulo 9/Ejercicio 103.1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ejercicio103._1;
+
+clsPersona[] personas = new clsPersona[5];
+
+Console.WriteLine("Ingrese los datos de la persona: ");
+
+for  (int i = 0; i < personas.Length; i++)
+{
+    personas[i] = new clsPersona();
+
+    Console.WriteLine("Nombre: ");
+    personas[i].Nombre = Console.ReadLine();
+
+    Console.WriteLine("Edad: ");
+    personas[i].Edad = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Las personas mayores de edad son: ");
+
+foreach(clsPersona persona in personas)
+{
+    if (persona.Edad >= 18)
+    {
+        Console.WriteLine(persona.Nombre);
+    }
+}
+
+
+Console.ReadKey();
