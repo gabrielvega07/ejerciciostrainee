d5bc15d9623ccb5c9869fe46b229e0cd43fd44d2
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-19 10:42:04 -0600

    Se presenta la solucion del ejercicio 103.6

---
+++ Semana 4/Modulo 9/Ejercicio 103.6/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ejercicio103._6;
+
+clsAnimal animal1 = new clsAnimal();
+animal1.Tipo = "Gato";
+animal1.ColorDePelo = "Blanco";
+animal1.Domestico = true;
+animal1.NumeroPatas = 4;
+animal1.Nombre = "Mito";
+
+
+Console.WriteLine($"Su nombre es: {animal1.Nombre}");
+Console.WriteLine($"Tipo: {animal1.Tipo}");
+Console.WriteLine($"Color de Pelo: {animal1.ColorDePelo}");
+Console.WriteLine($"Es Doméstico: {animal1.Domestico}");
+Console.WriteLine($"Número de Patas: {animal1.NumeroPatas}");
---
+++ Semana 4/Modulo 9/Ejercicio 103.6/clsAnimal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio103._6
+{
+    public class clsAnimal
+    {
+        public string Tipo { get; set; }
+        public string ColorDePelo { get; set; }
+        public bool Domestico { get; set; }
+        public int NumeroPatas { get; set; }
+        public string Nombre { get; set; }
+    }
+}
