194e874968d80a5178e680248275cef8ce91b2eb
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-19 10:33:41 -0600

    Se presenta la solucion del ejercicio 103.5

---
+++ Semana 4/Modulo 9/Ejercicio 103.5/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ejercicio103._5;
+
+int sumaPrecio = clsProducto.Sumar(10, 1);
+Console.WriteLine($"El resultado de la suma del precio y el impuesto del producto es {sumaPrecio}");
+
+Console.ReadLine();
---
+++ Semana 4/Modulo 9/Ejercicio 103.5/clsProducto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio103._5
+{
+    public static class clsProducto
+    {
+        public static int Sumar(double precio, double impuesto)
+        {
+            impuesto =  precio * 0.12;
+            return (int)(precio + impuesto);
+        }
+    }
+}
