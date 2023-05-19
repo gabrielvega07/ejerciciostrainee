66c8e05072e89247d8bb96c3a0977f586971f040
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-19 10:22:28 -0600

    Se presenta la solucion del ejercicio 103.4

---
+++ Semana 4/Modulo 9/Ejercicio 103.4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ejercicio103._4;
+
+clsVehiculo coche = new clsVehiculo(4, 4);
+Console.WriteLine($"Coche tiene: Ruedas = {coche.Ruedas}, Puertas = {coche.Puertas}");
+
+clsVehiculo avion = new clsVehiculo(3, 1);
+Console.WriteLine($"Avion tiene: Ruedas = {avion.Ruedas}, Puertas = {avion.Puertas}");
+
+clsVehiculo bicileta = new clsVehiculo(2);
+Console.WriteLine($"Bicicleta tiene: Ruedas = {bicileta.Ruedas}, Puertas = {bicileta.Puertas}");
+
+Console.ReadKey();
---
+++ Semana 4/Modulo 9/Ejercicio 103.4/clsVehiculo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio103._4
+{
+    public class clsVehiculo
+    {
+        private int ruedas, puertas;
+        public clsVehiculo()
+        {
+            ruedas = 0;
+            puertas = 0;
+        }
+
+        public clsVehiculo(int ruedas, int puertas)
+        {
+            this.ruedas = ruedas;   
+            this.puertas = puertas; 
+
+        }
+
+        public clsVehiculo(int ruedas)
+        {
+            this.ruedas = ruedas;
+            puertas = 0;
+        }
+
+        public int Ruedas
+        {
+            get { return ruedas; }
+            set { ruedas = value; }
+        }
+
+        public int Puertas
+        {
+            get { return puertas; }
+            set { puertas = value; }
+        }
+    }
+}
