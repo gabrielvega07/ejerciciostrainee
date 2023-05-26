ecab1e24ed8ae13f31fb3bbd05506d52e259ad96
gabrielvega07 <choj2015pablo@gmail.com>
2023-05-26 12:49:25 -0600

    Se presenta la solucion del ejercicio 125.2

---
+++ Semana 5/Modulo 11/Ejercicio 125.2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ejercicio125._2;
+
+clsListado<string> lista = new clsListado<string>();
+
+while (true)
+{
+    Console.WriteLine("Introducir un valor o salir para mostrar listado");
+    string valor = Console.ReadLine();
+    if (valor.ToLower() == "salir")
+        break;
+    lista.AgrgarValor(valor);
+}
+
+lista.MostrarLista();
+
+Console.ReadKey();
---
+++ Semana 5/Modulo 11/Ejercicio 125.2/clsListado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio125._2
+{
+    class clsListado<T>
+    {
+        private List<T> listado;
+
+        public clsListado()
+        {
+            listado = new List<T>();
+
+        }
+
+        public void AgrgarValor(T valor)
+        {
+            listado.Add(valor);
+        }
+
+        public void MostrarLista()
+        {
+            Console.WriteLine("Lista: ");
+            foreach (T valor in listado)
+            {
+                Console.WriteLine(valor);
+            }
+        }
+    }
+}
