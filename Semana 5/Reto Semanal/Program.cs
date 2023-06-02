1e25db4107f0e9db8153dbec25a4e08698966085
gabrielvega07 <choj2015pablo@gmail.com>
2023-06-02 09:35:02 -0600

    Se presenta la solucion del reto semanal 5

---
+++ Semana 5/Reto Semanal/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using EjerccioSemana5;
+
+Console.WriteLine("¡Bienvenido al juego de aventuras!");
+
+// Crear un nuevo jugador
+clsJugador jugador = CrearJugador();
+
+// Guardar los datos del jugador en un archivo binario
+Console.WriteLine("Guardando datos del jugador...");
+jugador.GuardarDatos("datosJugador.bin");
+
+// Cargar los datos del jugador desde un archivo binario
+Console.WriteLine("Cargando datos del jugador...");
+clsJugador jugadorCargado = clsJugador.CargarDatos("datosJugador.bin");
+
+// Verificar si se cargaron correctamente los datos del jugador
+if (jugadorCargado != null)
+{
+    Console.WriteLine("Datos del jugador cargados correctamente:");
+    Console.WriteLine($"Nombre: {jugadorCargado.Nombre}");
+    Console.WriteLine($"Nivel: {jugadorCargado.Nivel}");
+    Console.WriteLine($"Puntos de vida: {jugadorCargado.PuntosDeVida}");
+    Console.WriteLine($"Número de reinicios: {jugadorCargado.NumeroDeReinicios}");
+}
+
+Console.WriteLine("¡Fin del programa!");
+Console.ReadLine();
+
+static clsJugador CrearJugador()
+{
+    Console.WriteLine("Crear nuevo jugador");
+
+    Console.Write("Nombre: ");
+    string nombre = Console.ReadLine();
+
+    Console.Write("Nivel: ");
+    int nivel = int.Parse(Console.ReadLine());
+
+    Console.Write("Puntos de vida: ");
+    int puntosDeVida = int.Parse(Console.ReadLine());
+
+    Console.Write("Número de reinicios: ");
+    int numeroDeReinicios = int.Parse(Console.ReadLine());
+
+    return new clsJugador(nombre, nivel, puntosDeVida, numeroDeReinicios);
+
+}
---
+++ Semana 5/Reto Semanal/clsJugador.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerccioSemana5
+{
+    [Serializable]
+    public class clsJugador
+    {
+        public string Nombre { get; set; }
+        public int Nivel { get; set; }
+        public int PuntosDeVida { get; set; }
+        public int NumeroDeReinicios { get; set; }
+
+        // Constructor de la clase
+        public clsJugador(string nombre, int nivel, int puntosDeVida, int numeroDeReinicios)
+        {
+            Nombre = nombre;
+            Nivel = nivel;
+            PuntosDeVida = puntosDeVida;
+            NumeroDeReinicios = numeroDeReinicios;
+        }
+
+        // Método para guardar los datos del jugador en un archivo binario
+        public void GuardarDatos(string filePath)
+        {
+            try
+            {
+                // Crear una instancia del formateador binario
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                // Crear un flujo de salida para el archivo
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    // Serializar el objeto clsJugador y escribirlo en el archivo
+                    formatter.Serialize(stream, this);
+                }
+
+                Console.WriteLine("Datos del jugador guardados correctamente.");
+            }
+            catch (Exception ex)
+            {
+                // Mostrar mensaje de error al jugador
+                Console.WriteLine("Error al guardar los datos del jugador. Por favor, intenta nuevamente.");
+
+                // Registrar los detalles del error en un archivo de registro
+                string logFilePath = "error.log";
+                string errorMessage = $"Fecha y hora: {DateTime.Now}\nMensaje de error: {ex.Message}\nDetalles: {ex.StackTrace}\n";
+                File.AppendAllText(logFilePath, errorMessage);
+            }
+        }
+
+        // Método para cargar los datos del jugador desde un archivo binario
+        public static clsJugador CargarDatos(string filePath)
+        {
+            try
+            {
+                // Crear una instancia del formateador binario
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                // Verificar si el archivo existe
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("El archivo de datos del jugador no existe.");
+                    return null;
+                }
+
+                // Crear un flujo de entrada para el archivo
+                using (FileStream stream = new FileStream(filePath, FileMode.Open))
+                {
+                    // Deserializar el objeto clsJugador desde el archivo
+                    clsJugador jugador = (clsJugador)formatter.Deserialize(stream);
+                    return jugador;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Mostrar mensaje de error al jugador
+                Console.WriteLine("Error al cargar los datos del jugador. Por favor, intenta nuevamente.");
+
+                // Registrar los detalles del error en un archivo de registro
+                string logFilePath = "error.log";
+                string errorMessage = $"Fecha y hora: {DateTime.Now}\nMensaje de error: {ex.Message}\nDetalles: {ex.StackTrace}\n";
+                File.AppendAllText(logFilePath, errorMessage);
+
+                return null;
+            }
+        }
+
+        // Otros métodos y funcionalidades de la clase
+    }
+}
