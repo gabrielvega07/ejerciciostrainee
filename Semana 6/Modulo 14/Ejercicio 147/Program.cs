519a1468d8e220de6798b5d4ba552497a2ce7595
gabrielvega07 <choj2015pablo@gmail.com>
2023-06-02 09:55:30 -0600

    Se presenta la solucion del ejercicio modulo 14

---
+++ Semana 6/Modulo 14/Ejercicio 147/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ejercicio147;
+
+clsRecipe tortillaRecipe = new clsRecipe("Tortilla de Patatas");
+
+Console.WriteLine($"Receta: {tortillaRecipe.Name}");
+Console.WriteLine("Ingredientes:");
+foreach (var ingredient in tortillaRecipe.Ingredients)
+{
+    Console.WriteLine($"- {ingredient}");
+}
+
+Console.WriteLine("Preparando la receta...");
+await tortillaRecipe.PrepareRecipeAsync();
+
+Console.WriteLine("¡La tortilla de patatas está lista!");
+
+Console.ReadLine();
+
+
+
---
+++ Semana 6/Modulo 14/Ejercicio 147/clsRecipe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio147
+{
+    internal class clsRecipe
+    {
+        public string Name { get; }
+        public string[] Ingredients { get; }
+
+        public clsRecipe(string name)
+        {
+            Name = name;
+            Ingredients = new string[] { "patatas", "huevos", "cebolla", "aceite", "sal" };
+        }
+
+        public async Task PrepareRecipeAsync()
+        {
+            await PrepareIngredientsAsync();
+            await CookAsync();
+        }
+
+        private async Task PrepareIngredientsAsync()
+        {
+            Console.WriteLine("Pelando y cortando las patatas...");
+            await Task.Delay(1000);
+
+            Console.WriteLine("Cortando la cebolla...");
+            await Task.Delay(500);
+
+            Console.WriteLine("Batiendo los huevos...");
+            await Task.Delay(300);
+        }
+
+        private async Task CookAsync()
+        {
+            Console.WriteLine("Calentando el aceite en la sartén...");
+            await Task.Delay(1000);
+
+            Console.WriteLine("Añadiendo las patatas y la cebolla a la sartén...");
+            await Task.Delay(1500);
+
+            Console.WriteLine("Cocinando la tortilla por ambos lados...");
+            await Task.Delay(2000);
+        }
+
+    }
+}
